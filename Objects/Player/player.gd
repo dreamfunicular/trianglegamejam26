c08773387294ead2ce4c3d4c1c7e6c959f6585e6
Joe Maloney <118585970+Peas-N-Carrots@users.noreply.github.com>
2026-01-19 21:58:45 -0500

    anim start

--- Objects/Player/player.gd
+++ Objects/Player/player.gd
@@ -1,23 +1,26 @@
 extends CharacterBody3D
 
-const GRAV = -9.8
+const GRAV = -15.0
 
 const CAMERA_ROLL_PERCENT = 0.5
 
-const ROLL_SENSE = 0.001
+const ROLL_SENSE = 0.002
 const ROLL_NORMALIZE_SPEED = 4
 
 const MOUSE_SENSE = 0.003
 
 @onready var pitch_pivot = $PitchPivot
 @onready var camera = $PitchPivot/Camera3D
-@onready var model = $PitchPivot/Placeholder
+@onready var model = $PitchPivot/Gannet2
 
 # Camera settings (COME BACK TO THESE FOR SPEED STUFF)
 const CAMERA_DISTANCE = 5.0
 const CAMERA_HEIGHT = 1.0
 
 var roll : float = 0.0
+
+@export var flightBlendPath: String
+@export var flappingBlendPath: String
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -30,11 +33,14 @@ func _input(event) -> void:
 		rotate_y(-event.relative.x * MOUSE_SENSE)
 		pitch_pivot.rotate_x(-event.relative.y * MOUSE_SENSE)
 		roll += event.relative.x * ROLL_SENSE
-		#rotation.x = clamp(rotation.x, -PI/2, PI/2) # Uncomment to disable upside down flying
 
 func _physics_process(delta) -> void:
-	roll = clamp(lerp(roll, 0.0, 1.0 - exp(-ROLL_NORMALIZE_SPEED * delta)), -PI/8, PI/8)
-	model.rotation.x = roll
+	var wing_amount = clamp(1.2 - pitch_pivot.global_transform.basis.z.normalized().y * 1.4, 0.0, 1.0)
+	model.get_anim_tree().set(flightBlendPath, wing_amount);
+	model.get_anim_tree().set(flappingBlendPath, 0)
+	
+	roll = clamp(lerp(roll, 0.0, 1.0 - exp(-ROLL_NORMALIZE_SPEED * delta)), -PI/4, PI/4)
+	model.rotation.z = -roll
 	rotation.z = -roll * CAMERA_ROLL_PERCENT
 	
 	velocity.y += GRAV * delta
@@ -45,9 +51,9 @@ func _physics_process(delta) -> void:
 	var speed_weight = ease(min(velocity.length() / 20, 1.0), 0.3)
 	velocity = velocity.lerp(desired_dir * velocity.length(), speed_weight * 0.1 * 60 * delta)
 	
-	var dot_weight = clamp(velocity.normalized().dot(desired_dir.normalized()), 0.05, 1.0) * 0.1
+	#var dot_weight = velocity.normalized().dot(desired_dir.normalized())
 	
 	if Input.is_action_just_pressed("ui_accept"):
-		velocity = -transform.basis.z * 30.0
+		velocity = (-transform.basis.z + pitch_pivot.transform.basis.y).normalized() * 30.0
 	
 	move_and_slide()
