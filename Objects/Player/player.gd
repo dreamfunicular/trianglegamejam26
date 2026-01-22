ad06d0d4981e9f086a27b1e316483e369756ecff
Camden MacIver <camdenmac@icloud.com>
2026-01-22 00:05:25 -0500

    did some more touching up. this thing will need to be re felt out a lot lol

--- Objects/Player/player.gd
+++ Objects/Player/player.gd
@@ -22,7 +22,8 @@ var roll : float = 0.0
 @export var flightBlendPath: String
 @export var flappingBlendPath: String
 
-var in_water : bool = false
+var bird_in_water : bool = false
+var camera_in_water : bool = false
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -36,7 +37,24 @@ func _input(event) -> void:
 		pitch_pivot.rotate_x(-event.relative.y * MOUSE_SENSE)
 		roll += event.relative.x * ROLL_SENSE
 
+func update_state():
+	if (position.y < 0 && not bird_in_water):
+		bird_enter_water()
+	
+	if (position.y > 0 && bird_in_water):
+		bird_exit_water()
+	
+	if (camera.global_position.y < 0 && not camera_in_water):
+		camera_enter_water()
+	
+	if (camera.global_position.y > 0 && camera_in_water):
+		camera_exit_water()
+
+
 func _physics_process(delta) -> void:
+	# updates all info about where the bird and the camera are
+	update_state()
+		
 	var wing_amount = clamp(1.2 - pitch_pivot.global_transform.basis.z.normalized().y * 1.4, 0.0, 1.0)
 	model.get_anim_tree().set(flightBlendPath, wing_amount);
 	model.get_anim_tree().set(flappingBlendPath, 0)
@@ -45,29 +63,41 @@ func _physics_process(delta) -> void:
 	model.rotation.z = -roll
 	rotation.z = -roll * CAMERA_ROLL_PERCENT
 	
-	velocity.y += GRAV * delta
 	
-	var desired_dir = -pitch_pivot.global_transform.basis.z
-	
-	#var speed_weight = clamp(20.0 / max(velocity.length(), 1.0), 0.05, 1.0) * 0.9
-	var speed_weight = ease(min(velocity.length() / 20, 1.0), 0.3)
-	velocity = velocity.lerp(desired_dir * velocity.length(), speed_weight * 0.1 * 60 * delta)
-	
-	#var dot_weight = velocity.normalized().dot(desired_dir.normalized())
-	
-	if Input.is_action_just_pressed("ui_accept"):
-		velocity = (-transform.basis.z + pitch_pivot.transform.basis.y).normalized() * 30.0
-	
-	
-	
-	if (position.y < 0 && not in_water):
-		enter_water()
+	# flying bird only code
+	if not bird_in_water: 
 		
-	if (position.y > 0 && in_water):
-		exit_water()
-	
-	if in_water:
-		velocity.y = min(velocity.y + 1.5, 100)
+		velocity.y += GRAV * delta
+		
+		var desired_dir = -pitch_pivot.global_transform.basis.z
+		
+		#var speed_weight = clamp(20.0 / max(velocity.length(), 1.0), 0.05, 1.0) * 0.9
+		var speed_weight = ease(min(velocity.length() / 20, 1.0), 0.3)
+		
+		var divespeed_coefficient = 1
+		if (desired_dir.normalized().y > 0):
+			divespeed_coefficient = 1 + desired_dir.normalized().y * 0.7
+		
+		velocity = velocity.lerp(desired_dir * velocity.length(), divespeed_coefficient * speed_weight * 0.1 * 60 * delta)
+		
+		
+		
+		#var dot_weight = velocity.normalized().dot(desired_dir.normalized())
+		
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity = (-transform.basis.z + pitch_pivot.transform.basis.y).normalized() * 30.0
+			
+		
+		
+	# water bird
+	else: 
+		pass
+		var pitch = pitch_pivot.global_transform.basis.z.normalized().y # -1 is straight up, 1 is straight down
+		velocity.y += pow(-position.y, 1.4)/150 * (6-pow(pitch+2, 1.4))
+		clampf(velocity.y, 0, 50)
+		# this math is kinda temp still but i like the vibe of it.
+		# basically the longer you stay pointed down the longer you go
+		# tthink about it like variable height jump from mario
 		
 	move_and_slide()
 
@@ -76,8 +106,14 @@ func set_shader_value(param: String, value):
 	$UnderwaterEffect.get_child(0).material.set_shader_parameter(param, value);
 
 
-func enter_water():
-	in_water = true
+func bird_enter_water():
+	bird_in_water = true
+
+func bird_exit_water():
+	bird_in_water = false
+
+func camera_enter_water():
+	camera_in_water = true
 	
 	var tween = get_tree().create_tween()
 	tween.set_ease(tween.EASE_IN) # idk if this does anything
@@ -88,8 +124,8 @@ func enter_water():
 	
 	# do any sound adjustments
 
-func exit_water():
-	in_water = false
+func camera_exit_water():
+	camera_in_water = false
 	
 	var tween = get_tree().create_tween()
 	tween.set_ease(tween.EASE_IN)
