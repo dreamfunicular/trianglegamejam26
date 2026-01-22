6993165828ed0969252452b4ca01d31f735a8e48
Camden MacIver <camdenmac@icloud.com>
2026-01-21 20:32:37 -0500

    added some more stuff to the underwater effect

--- testing/underwater_effect.gd
+++ testing/underwater_effect.gd
@@ -1,23 +1,39 @@
 extends CanvasLayer
 
+var in_water :bool = false
 
 func set_shader_value(param: String, value):
-	# in my case i'm tweening a shader on a texture rect, but you can use anything with a material on it
-	#var value = $Shader.material.get_shader_parameter(param)
 	$Shader.material.set_shader_parameter(param, value);
 
 
 func enter_water():
+	in_water = true
+	
+	var tween = get_tree().create_tween()
+	tween.set_ease(tween.EASE_IN) # idk if this does anything
+	
+	tween.tween_method(func(value): set_shader_value("activate", value), 0.0, 1.0, 0.1); # args are: (method to call / start value / end value / duration of animation)
+	
+	## TODO play any sound effect
+	
+	# do any sound adjustments
+
+func exit_water():
+	in_water = false
+	
 	var tween = get_tree().create_tween()
 	tween.set_ease(tween.EASE_IN)
+	tween.tween_method(func(value): set_shader_value("activate", value), 1.0, 0.0, 0.1); # args are: (method to call / start value / end value / duration of animation)
 	
-	#start_val = $Shader.material.get_shader_parameter(param)
-	tween.tween_method(func(value): set_shader_value("activate", value), 0.1, 1.0, 0.1); # args are: (method to call / start value / end value / duration of animation)
-
+	## TODO play any sound effect
 	
+	# do any sound adjustments
 	
-func _process(float) -> void:
-	pass
+func _process(_delta) -> void:
 	if (Input.is_action_just_released("ui_accept")):
-		enter_water()
+		if not in_water:
+			enter_water()
+			
+		else:
+			exit_water()
 	
