cd8426593524557a54dc8edcaa15bfa5144001c0
Joe Maloney <118585970+Peas-N-Carrots@users.noreply.github.com>
2026-01-26 15:53:42 -0500

    GUI debugged/tested/improved

--- GUI/Score/score.gd
+++ GUI/Score/score.gd
@@ -3,8 +3,12 @@ extends VBoxContainer
 var score : int = 0
 var display_score : int = 0
 
+func change_score(amount: int) -> void:
+	score += amount
+	if score < 0: score = 0
+
 func _process(delta: float) -> void:
 	@warning_ignore("narrowing_conversion")
 	display_score = move_toward(display_score, score, int(60 * delta * 3))
 	
-	$Display.text = "%4d" % display_score
+	$Display.text = "%04d" % display_score
--- GUI/Stamina Bar/stamina.gd
+++ GUI/Stamina Bar/stamina.gd
@@ -1,6 +1,7 @@
 extends Container
 
 var stamina: float = 10.0
+var display_stamina : float = 10.0
 
 func use_stamina(amount: float) -> bool:
 	if stamina - amount >= 0.0:
@@ -9,5 +10,6 @@ func use_stamina(amount: float) -> bool:
 	return false
 
 func _process(delta: float) -> void:
-	stamina = move_toward(stamina, 10.0, delta * 60 * 0.1)
-	$AnimatedSprite2D.frame = clamp(floor(stamina * 2), 0, 20)
+	stamina = move_toward(stamina, 10.0, delta * 60 * 0.01)
+	display_stamina = lerp(display_stamina, stamina, 60 * delta * 0.5)
+	$AnimatedSprite2D.frame = clamp(floor(display_stamina * 2), 0, 20)
